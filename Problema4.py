63b16f110588b09f99eb2753ab8ab8acae43dfab
Nicolas126 <69853391+Nicolas126@users.noreply.github.com>
2020-10-05 12:17:55 -0300

    Update Problema4.py

--- Problema4.py
+++ Problema4.py
@@ -1,8 +1,9 @@
-num01 = int(input('Digite um valor:'))
-num02 = int(input('Digite um valor:'))
-if num01 == num02:
-    print('Os valores digitados  são iguais')
-elif num01 > num02:
-    print('Maior número: {}'.format(num01))
-elif num02 > num01:
-    print('Maior número: {}'.format(num02))
+n1 = float(input('Digite o primeiro número:'))
+n2 = float(input('Digite o segundo número:'))
+
+if n1 > n2:
+    print(f'O número {n1} é maior que {n2}')
+if n2 > n1:
+    print(f'O número {n2} é maior que {n1}')
+else:
+    print(f'Os números {n1} {n2} são iguais')
